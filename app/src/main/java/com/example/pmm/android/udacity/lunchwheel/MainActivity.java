38fca0efb665dd9fc47942161f7e5c716b8dd3bd
Patrick Monkelban <pmonkelban@gmail.com>
2015-09-24 00:18:11 -0400

    Receiving Location Data

--- app/src/main/java/com/example/pmm/android/udacity/lunchwheel/MainActivity.java
+++ app/src/main/java/com/example/pmm/android/udacity/lunchwheel/MainActivity.java
@@ -1,16 +1,55 @@
 package com.example.pmm.android.udacity.lunchwheel;
 
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+public class MainActivity extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    public static final int LOCATION_UPDATE_INTERVAL = 1000;  // in milliseconds
+
+    private TextView mOutputTextView;
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+        mOutputTextView = (TextView) findViewById(R.id.txtOutput);
+    }
+
+    @Override
+    protected void onStart()  {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop()  {
+        mGoogleApiClient.disconnect();
+        super.onStop();
     }
 
     @Override
@@ -33,5 +72,46 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.i(TAG, "onConnected() called");
+
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(LOCATION_UPDATE_INTERVAL);
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocationRequest, this);
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(TAG, "onConnectionSuspended() called");
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        String latLon = "";
+
+        if (location != null)  {
+            latLon = "Lat:" + location.getLatitude() + " Lon:" + location.getLongitude();
+        }
+
+        Log.d(TAG, "onLocationChanged() called. " + latLon);
+
+        mOutputTextView.setText(latLon);
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+        Log.e(TAG, "onConnectionFailed() called");
+
     }
 }
