7b85e30e323200b5d7be433807275d7fa6400730
Patrick Monkelban <pmonkelban@gmail.com>
2015-10-03 00:53:52 -0400

    Added Spin button and results

--- app/src/main/java/com/example/pmm/android/udacity/lunchwheel/MainActivity.java
+++ app/src/main/java/com/example/pmm/android/udacity/lunchwheel/MainActivity.java
@@ -13,13 +13,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.pmm.android.udacity.lunchwheel.data.DataContract;
+import com.example.pmm.android.udacity.lunchwheel.data.DataProvider;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity
         implements
@@ -39,12 +44,19 @@ public class MainActivity extends AppCompatActivity
 
     private ListView mListView;
     private WheelAdapter mWheelAdapter;
+    private Button mSpinButton;
 
     public static final int RESTAURANT_LOADER_ID = 0;
+
+    Random rnd;
+
+//    public static final double REFRESH_DISTANCE_METERS = 100d;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        rnd = new Random(System.currentTimeMillis());
 
         /*
         * Set the location mode to device if it hasn't been set yet.
@@ -77,6 +89,45 @@ public class MainActivity extends AppCompatActivity
         mListView = (ListView) findViewById(R.id.restaurants_listView);
         mListView.setAdapter(mWheelAdapter);
         getLoaderManager().initLoader(RESTAURANT_LOADER_ID, null, this);
+
+        mSpinButton = (Button) findViewById(R.id.spin_wheel_button);
+        mSpinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doSpinWheel();
+            }
+        });
+
+
+    }
+
+    private void doSpinWheel() {
+
+        Cursor c = getApplicationContext().getContentResolver().query(
+                DataContract.RestaurantEntry.CONTENT_URI,
+                null,
+                null,
+                null,
+                null);
+
+        if (c.getCount() == 0)  {
+            Toast.makeText(
+                    getApplicationContext(),
+                    getString(R.string.toast_no_results),
+                    Toast.LENGTH_SHORT)
+                    .show();
+            return;
+        }
+
+        int randomPositon = rnd.nextInt(c.getCount());
+
+        c.moveToPosition(randomPositon);
+
+        String winner = getString(R.string.toast_announce_winner) +
+                System.getProperty("line.separator") +
+                c.getString(DataProvider.RESTAURANT_INDEX_NAME);
+
+        Toast.makeText(getApplicationContext(), winner, Toast.LENGTH_LONG).show();
 
     }
 
@@ -160,23 +211,35 @@ public class MainActivity extends AppCompatActivity
 
     private void updateLocation() {
 
-        String locationMode = prefs.getString(Constants.PREF_LOCATION_MODE, "");
+//        float old_lat = prefs.getFloat(Constants.PREF_DEVICE_LATITUDE, 0f);
+//        float old_lon = prefs.getFloat(Constants.PREF_DEVICE_LONGITUDE, 0f);
 
-        if (Constants.PREF_LOCATION_MODE_DEVICE.equals(locationMode)) {
+        float lon = (mLastDeviceLocation == null) ? 0f : (float) mLastDeviceLocation.getLongitude();
+        float lat = (mLastDeviceLocation == null) ? 0f : (float) mLastDeviceLocation.getLatitude();
 
-            prefs.edit()
-                    .putFloat(Constants.PREF_DEVICE_LATITUDE, (float) mLastDeviceLocation.getLatitude())
-                    .putFloat(Constants.PREF_DEVICE_LONGITUDE, (float) mLastDeviceLocation.getLongitude())
-                    .commit();
+        prefs.edit()
+                .putFloat(Constants.PREF_DEVICE_LATITUDE, lat)
+                .putFloat(Constants.PREF_DEVICE_LONGITUDE, lon)
+                .commit();
 
-            if (Constants.PREF_LOCATION_MODE_DEVICE.equals(
-                    prefs.getString(Constants.PREF_LOCATION_MODE, ""))) {
+        if (Constants.PREF_LOCATION_MODE_DEVICE.equals(
+                prefs.getString(Constants.PREF_LOCATION_MODE, ""))) {
 
-                SearchService.updateSearchResults(this);
+            if (mLastDeviceLocation == null) {
+                Toast.makeText(
+                        getApplicationContext(),
+                        getString(R.string.toast_no_device_location),
+                        Toast.LENGTH_LONG)
+                        .show();
+            } else {
+
+//                if (Util.distance(old_lat, lat, old_lon, lon, 0d, 0d) > REFRESH_DISTANCE_METERS) {
+                    SearchService.updateSearchResults(this);
+//                }
             }
         }
-
     }
+
 
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
