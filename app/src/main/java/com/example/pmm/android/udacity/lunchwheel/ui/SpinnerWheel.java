008738117b38f698f0229310ad9e8e2ed1de87dc
Patrick Monkelban <pmonkelban@gmail.com>
2015-10-10 11:38:15 -0400

    In Progress

--- app/src/main/java/com/example/pmm/android/udacity/lunchwheel/ui/SpinnerWheel.java
+++ app/src/main/java/com/example/pmm/android/udacity/lunchwheel/ui/SpinnerWheel.java
@@ -1,39 +1,12 @@
 package com.example.pmm.android.udacity.lunchwheel.ui;
 
-import android.app.ListActivity;
-import android.app.LoaderManager;
 import android.content.Context;
-import android.content.Loader;
-import android.content.res.TypedArray;
-import android.database.Cursor;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.drawable.Drawable;
-import android.os.Bundle;
-import android.text.TextPaint;
 import android.util.AttributeSet;
 import android.view.View;
 
-import com.example.pmm.android.udacity.lunchwheel.R;
+public class SpinnerWheel extends View  {
 
-/**
- * TODO: document your custom view class.
- */
-public class SpinnerWheel extends ListActivity implements LoaderManager.LoaderCallbacks<Cursor>  {
-
-    @Override
-    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        return null;
-    }
-
-    @Override
-    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-
-    }
-
-    @Override
-    public void onLoaderReset(Loader<Cursor> loader) {
-
+    public SpinnerWheel(Context context, AttributeSet attrs)  {
+        super(context, attrs);
     }
 }
